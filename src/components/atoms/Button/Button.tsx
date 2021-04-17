bd6569924d55d8e7ef73f6944c4096ab6596d0b9
janossik <marcinczaniecki97@gmail.com>
2021-04-17 23:58:09 +0200

    Hero, Box, Button, edit Menu
    Create Hero and the necessary components. Changing the logo, adding the font size to the theme

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  position: relative;
+  display: flex;
+  height: 58px;
+  min-width: 135px;
+  margin-right: auto;
+  padding-bottom: 5px;
+  font-size: ${({ theme }) => theme.font.size.l};
+  font-weight: ${({ theme }) => theme.font.weight.bold};
+  color: ${({ theme }) => theme.color.primary};
+  text-decoration: none;
+  align-items: center;
+  justify-content: center;
+  border: solid 4px ${({ theme }) => theme.color.primary};
+  border-radius: 100px;
+  transition: 300ms;
+  &:hover {
+    color: ${({ theme }) => theme.color.background};
+    background-color: ${({ theme }) => theme.color.primary};
+  }
+`;
+
+export default Button;
---
+++ src/components/molecules/Box/Box.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import Title from "components/atoms/Title/Title";
+import Button from "components/atoms/Button/Button";
+import { IBox } from "interface/interface";
+
+const Wrapper = styled.section`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+`;
+
+const Box = ({ title, children, url, linkText }: IBox) => {
+  return (
+    <Wrapper>
+      <Title tag="h2" primary>
+        {title}
+      </Title>
+      <p>{children}</p>
+      {url && (
+        <Button href={url} as="a">
+          {linkText}
+        </Button>
+      )}
+    </Wrapper>
+  );
+};
+
+export default Box;
