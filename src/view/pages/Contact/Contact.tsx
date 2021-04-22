7e762e38bfa18571ee6b0c5addd102d5a1477543
janossik <marcinczaniecki97@gmail.com>
2021-04-22 23:25:35 +0200

    Update Contact.tsx

--- src/view/pages/Contact/Contact.tsx
+++ src/view/pages/Contact/Contact.tsx
@@ -1,14 +1,13 @@
 import styled from "styled-components";
 import Box from "components/molecules/Box/Box";
-//import Button from "components/atoms/Button/Button";
-import NetlifyForm from "react-netlify-form";
+import Button from "components/atoms/Button/Button";
 
 const Wrapper = styled.article`
   display: flex;
   flex-direction: column;
   gap: 20px;
 `;
-/* 
+
 const Form = styled.form`
   display: flex;
   flex-direction: column;
@@ -32,7 +31,7 @@ const Textarea = styled.textarea`
   padding: 20px 30px;
   border: 3px solid ${({ theme }) => theme.color.primary};
   border-radius: 30px;
-`; */
+`;
 
 const Contact = () => {
   return (
@@ -41,24 +40,21 @@ const Contact = () => {
         Napisz do mnie, jeżeli chcesz nawiązać współpracę
       </Box>
       <div>e-mail: marcinczaniecki97@gmail.com</div>
-      <NetlifyForm name="Contact Form">
-        {({ loading, error, success }) => (
-          <div>
-            {loading && <div>Loading...</div>}
-            {error && (
-              <div>Your information was not sent. Please try again later.</div>
-            )}
-            {success && <div>Thank you for contacting us!</div>}
-            {!loading && !success && (
-              <div>
-                <input type="text" name="Name" required />
-                <textarea name="Message" required />
-                <button>Submit</button>
-              </div>
-            )}
-          </div>
-        )}
-      </NetlifyForm>
+      <Form name="contact" method="post" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+        <Label>
+          Twój e-mail:
+          <Input type="email" name="email" placeholder="e-mail@xyz.com" />
+        </Label>
+        <Label>
+          Twoja Wiadomość:
+          <Textarea
+            name="content"
+            placeholder="W czym mogę Ci pomóc?"
+          ></Textarea>
+        </Label>
+        <Button type="submit">Wyślij</Button>
+      </Form>
     </Wrapper>
   );
 };
