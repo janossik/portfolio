b8667725c7019fbe9af9ed6bb6a4068bd8f9ee99
janossik <marcinczaniecki97@gmail.com>
2021-04-29 15:52:42 +0200

    Update Modal.tsx

--- src/components/organism/Modal/Modal.tsx
+++ src/components/organism/Modal/Modal.tsx
@@ -1,14 +1,11 @@
-import { useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import CloseButton from 'components/atoms/CloseButton/CloseButton';
-import { IModal } from 'types/types';
-import { Wrapper, Background } from './Modal.styles';
-
+import { useEffect } from "react";
+import ReactDOM from "react-dom";
+import CloseButton from "components/atoms/CloseButton/CloseButton";
+import { IModal } from "types/types";
+import { Wrapper, Background } from "./Modal.styles";
 
 const modalContainer = document.getElementById("modal-container");
-
-const Modal = ({ children, active, setActive }: IModal) => {
-  const modalNode = document.createElement("div")
+const modalNode = document.createElement("div");
 
 const Modal = ({ children, active, setActive }: IModal) => {
   useEffect(() => {
